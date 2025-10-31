6b900e94498deaccae91c31400240ac0cfde915f
malaonda222 <bandinelli.lisa@gmail.com>
2025-10-31 11:11:08 +0100

    commit

--- SIATE_playground/src/main.jsx
+++ SIATE_playground/src/main.jsx
@@ -4,12 +4,8 @@ import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App/>);
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
